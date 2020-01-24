6efcfc354292036cf00ab849e5475b3b6e584cd5
Grant Blakeman <gblakeman@linkedin.com>
2020-01-24 14:47:46 -0800

    crawler - convert some “layer” language to “node”

--- src/Crawler.ts
+++ src/Crawler.ts
@@ -2,8 +2,8 @@ import { CONTAINER_NODE_TYPES } from './constants';
 
 /**
  * @description A class to handle traversing an array of selected items and return useful items
- * (child layers, first in selection, layer position, dimensions and position of layer gaps
- * and overlapping layer negative space).
+ * (child nodes, first in selection, node position, dimensions and position of node gaps
+ * and overlapping node negative space).
  *
  * @class
  * @name Crawler
@@ -23,14 +23,14 @@ export default class Crawler {
    *
    * @kind function
    * @name first
-   * @returns {Object} The first layer item in the array.
+   * @returns {Object} The first node item in the array.
    */
   first() {
     return this.array[0];
   }
 
   /**
-   * @description Looks into the selection array for any groups and pulls out individual layers,
+   * @description Looks into the selection array for any groups and pulls out individual nodes,
    * effectively flattening the selection.
    *
    * @kind function
@@ -43,23 +43,23 @@ export default class Crawler {
     const flatSelection = [];
 
     // iterate through initial selection
-    initialSelection.forEach((layer: any) => {
+    initialSelection.forEach((node: any) => {
       if (
-        layer.type !== CONTAINER_NODE_TYPES.group
-        && layer.type !== CONTAINER_NODE_TYPES.frame
-        && layer.type !== CONTAINER_NODE_TYPES.component
-        && layer.type !== CONTAINER_NODE_TYPES.instance
-        && layer.visible
+        node.type !== CONTAINER_NODE_TYPES.group
+        && node.type !== CONTAINER_NODE_TYPES.frame
+        && node.type !== CONTAINER_NODE_TYPES.component
+        && node.type !== CONTAINER_NODE_TYPES.instance
+        && node.visible
       ) {
-        // non-frame or -group layers get added to the final selection
-        flatSelection.push(layer);
+        // non-frame or -group nodes get added to the final selection
+        flatSelection.push(node);
       } else {
-        // +++ frames and groups are checked for child layers
+        // +++ frames and groups are checked for child nodes
 
         // set initial holding array and add first level of children
         let innerLayers = [];
-        if (layer.visible) {
-          layer.children.forEach((child) => {
+        if (node.visible) {
+          node.children.forEach((child) => {
             if (child.visible) {
               innerLayers.push(child);
             }
@@ -67,8 +67,8 @@ export default class Crawler {
         }
 
         /**
-         * @description Iterates through `innerLayers`, adding normal layers to the `flatSelection`
-         * array, while adding any additional children layers into the `innerLayers` array.
+         * @description Iterates through `innerLayers`, adding normal nodes to the `flatSelection`
+         * array, while adding any additional children nodes into the `innerLayers` array.
          *
          * @kind function
          * @name iterateKnownChildren
@@ -77,8 +77,8 @@ export default class Crawler {
          * @private
          */
         const iterateKnownChildren = (): void => {
-          // iterate through known child layers in `innerLayers`,
-          // adding more children to the array as they are found in descendent layers
+          // iterate through known child nodes in `innerLayers`,
+          // adding more children to the array as they are found in descendent nodes
           innerLayers.forEach((
             innerLayer: {
               children: any,
@@ -94,7 +94,7 @@ export default class Crawler {
               && innerLayer.type !== CONTAINER_NODE_TYPES.instance
               && innerLayer.visible
             ) {
-              // non-frame or -group layers get added to the final selection
+              // non-frame or -group nodes get added to the final selection
               flatSelection.push(innerLayer);
             } else if (innerLayer.visible) {
               innerLayer.children.forEach((child) => {
@@ -104,7 +104,7 @@ export default class Crawler {
               });
             }
 
-            // update the overall list of child layers, removing the layer that was just examined.
+            // update the overall list of child nodes, removing the node that was just examined.
             // this array should eventually be empty, breaking the `while` loop
             const innerLayerIndex = innerLayers.findIndex(
               foundInnerLayer => (foundInnerLayer.id === innerLayer.id),
@@ -129,7 +129,7 @@ export default class Crawler {
   }
 
   /**
-   * @description Looks into the selection array for any groups and pulls out individual layers,
+   * @description Looks into the selection array for any groups and pulls out individual nodes,
    * effectively flattening the selection.
    *
    * @kind function
@@ -138,8 +138,8 @@ export default class Crawler {
    * @returns {Object} All items (including children) individual in an updated array.
    */
   allSorted() {
-    // start with flattened selection of all layers
-    const layers = this.all();
+    // start with flattened selection of all nodes
+    const nodes = this.all();
 
     // sort by `y` position and then `x` if `y` values are equal
     const sortByPosition = (nodeA, nodeB) => {
@@ -152,30 +152,30 @@ export default class Crawler {
       return aPos.y - bPos.y;
     };
 
-    const sortedLayers = layers.sort(sortByPosition);
+    const sortedLayers = nodes.sort(sortByPosition);
     return sortedLayers;
   }
 
   /**
    * @description Looks into the selection array for any groups and pulls out
-   * individual TextNode layers.
+   * individual TextNode nodes.
    *
    * @kind function
    * @name text
-   * @param {boolean} includeLocked Determines whether or not locked layers are included
+   * @param {boolean} includeLocked Determines whether or not locked nodes are included
    * in the selection.
    *
    * @returns {Array} All TextNode items in an array.
    */
   text(includeLocked: boolean = false): Array<TextNode> {
-    // start with flattened selection of all layers, ordered by position on the artboard
-    const layers = this.allSorted();
+    // start with flattened selection of all nodes, ordered by position on the artboard
+    const nodes = this.allSorted();
 
     // filter and retain immediate text nodes
-    let textNodes: Array<TextNode> = layers.filter((node: SceneNode) => node.type === 'TEXT');
+    let textNodes: Array<TextNode> = nodes.filter((node: SceneNode) => node.type === 'TEXT');
 
     // iterate through components to find additional text nodes
-    const componentNodes: Array<ComponentNode | InstanceNode> = layers.filter(
+    const componentNodes: Array<ComponentNode | InstanceNode> = nodes.filter(
       (node: SceneNode) => (
         (node.type === 'COMPONENT' || node.type === 'INSTANCE') && node.visible
       ),
