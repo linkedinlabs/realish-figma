e828c70d6c61df5e47b947f1c8c8b7fb32040a95
Grant Blakeman <hello@grantblakeman.com>
2020-08-07 13:20:41 -0600

    data - add image flag for company names

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -6,47 +6,176 @@
  * @type {Array}
  */
 export default [
-  'ABC Advertising',
-  'ABC Products',
-  'ABC Stores',
-  'Acme Construction',
-  'Acme Hotels',
-  'Acme Printing',
-  'Acme Productions',
-  'Acme Properties',
-  'Alpha Co.',
-  'Alpha Flight School',
-  'Alpha Maintenance Co.',
-  'Antelith',
-  'Central Hospital',
-  'Codelane',
-  'Cortez Travels',
-  'Energence',
-  'Fixdex',
-  'Flexis',
-  'Freshing',
-  'Funorama Co.',
-  'Gamma Incorporated',
-  'GoldenPhase',
-  'Itkix',
-  'Lamcane',
-  'Mintome',
-  'My own business',
-  'Omega Company',
-  'Omega Signs',
-  'Omega Travel',
-  'Oustia',
-  'Paradise Landscaping',
-  'Prime Co.',
-  'Prime Veterinary',
-  'Runity',
-  'Sage & Spice Restaurant',
-  'Self-employed',
-  'Techcore',
-  'Telendo',
-  'Trustway',
-  'Venofase',
-  'Viyo',
-  'Whistlety',
-  'Zoomjax',
+  {
+    name: 'ABC Advertising',
+    hasImage: false,
+  },
+  {
+    name: 'ABC Products',
+    hasImage: false,
+  },
+  {
+    name: 'ABC Stores',
+    hasImage: false,
+  },
+  {
+    name: 'Acme Construction',
+    hasImage: false,
+  },
+  {
+    name: 'Acme Hotels',
+    hasImage: false,
+  },
+  {
+    name: 'Acme Printing',
+    hasImage: false,
+  },
+  {
+    name: 'Acme Productions',
+    hasImage: false,
+  },
+  {
+    name: 'Acme Properties',
+    hasImage: false,
+  },
+  {
+    name: 'Alpha Co.',
+    hasImage: false,
+  },
+  {
+    name: 'Alpha Flight School',
+    hasImage: false,
+  },
+  {
+    name: 'Alpha Maintenance Co.',
+    hasImage: false,
+  },
+  {
+    name: 'Antelith',
+    hasImage: true,
+  },
+  {
+    name: 'Central Hospital',
+    hasImage: false,
+  },
+  {
+    name: 'Codelane',
+    hasImage: true,
+  },
+  {
+    name: 'Cortez Travels',
+    hasImage: false,
+  },
+  {
+    name: 'Energence',
+    hasImage: true,
+  },
+  {
+    name: 'Fixdex',
+    hasImage: true,
+  },
+  {
+    name: 'Flexis',
+    hasImage: true,
+  },
+  {
+    name: 'Freshing',
+    hasImage: true,
+  },
+  {
+    name: 'Funorama Co.',
+    hasImage: false,
+  },
+  {
+    name: 'Gamma Incorporated',
+    hasImage: false,
+  },
+  {
+    name: 'GoldenPhase',
+    hasImage: true,
+  },
+  {
+    name: 'Itkix',
+    hasImage: true,
+  },
+  {
+    name: 'Lamcane',
+    hasImage: true,
+  },
+  {
+    name: 'Mintome',
+    hasImage: true,
+  },
+  {
+    name: 'My own business',
+    hasImage: false,
+  },
+  {
+    name: 'Omega Company',
+    hasImage: false,
+  },
+  {
+    name: 'Omega Signs',
+    hasImage: false,
+  },
+  {
+    name: 'Omega Travel',
+    hasImage: false,
+  },
+  {
+    name: 'Oustia',
+    hasImage: true,
+  },
+  {
+    name: 'Paradise Landscaping',
+    hasImage: false,
+  },
+  {
+    name: 'Prime Co.',
+    hasImage: false,
+  },
+  {
+    name: 'Prime Veterinary',
+    hasImage: false,
+  },
+  {
+    name: 'Runity',
+    hasImage: true,
+  },
+  {
+    name: 'Sage & Spice Restaurant',
+    hasImage: false,
+  },
+  {
+    name: 'Self-employed',
+    hasImage: false,
+  },
+  {
+    name: 'Techcore',
+    hasImage: false,
+  },
+  {
+    name: 'Telendo',
+    hasImage: true,
+  },
+  {
+    name: 'Trustway',
+    hasImage: true,
+  },
+  {
+    name: 'Venofase',
+    hasImage: true,
+  },
+  {
+    name: 'Viyo',
+    hasImage: true,
+  },
+  {
+    name: 'Whistlety',
+    hasImage: true,
+  },
+  {
+    name: 'Zoomjax',
+    hasImage: true,
+  },
 ];
