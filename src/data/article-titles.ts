c400b8c2e6d0dea33274542247183874b0ddc3f1
Grant Blakeman <hello@grantblakeman.com>
2020-08-21 14:34:31 -0600

    data - add “articleTitles” set

---
+++ src/data/article-titles.ts
@@ -0,0 +1,48 @@
+/**
+ * @description Article titles.
+ *
+ * @kind constant
+ * @name articleTitles
+ * @type {Array}
+ */
+export default [
+  '20 Tips For Making Your Best Sales Pitch',
+  '3 Key Insights On Freshing’s Logistics Network',
+  'Antelith Commits $2B To Ocean Cleanup',
+  'CanAir Adds Several International Routes To Schedule',
+  'Codelane Is Sponsoring The Largest Student Code-athon Ever',
+  'Congratulations, You’re Done With School! Now What?',
+  'Designing Experiences For Our Next Reality',
+  'Fixdex Settles $200M Lawsuit Over Fake Accounts',
+  'GoldenPhase Closes Deal On 200 Acres Near Bellair Cliffs',
+  'Graduates Of Southern Hall University’s Healthcare Program Post Exceed National Average',
+  'How Codelane Is Paving The Way For Better Data Management',
+  'How Energence Is Leading The Way To A Better Solar',
+  'How Mintome Medical Sensors Are Saving Lives',
+  'How Multitasking Has Reduced Our Capacity To Remember',
+  'How Third Grade English Can Help You Become A Better Writer',
+  'Is Mental Health Suffering In The Workplace?',
+  'Itkix Provides Over 5,000 Thermal Blankets For Victims Of Hurricane Whitson',
+  'Leading In Every Healthcare Setting',
+  'Learning The Principles Of Gestalt',
+  'Retail Stores Black Friday Sales May Actually Be Bad Business',
+  'Runity Declares All Products Free Of Animal Testing',
+  'Taking Your Team International (And What You Should Know)',
+  'Telendo To Launch New Software That Automatically Files Your Taxes',
+  'The 4-Day Work Week Is Coming',
+  'The Art Of Social Selling',
+  'The Leadership Alphabet',
+  'The Top 50 Women In Business',
+  'The Wonders Of Modern Health',
+  'Turning Pro in Photography: The Basics',
+  'Viyo Goes Green',
+  'Viyo To Test New SMART Vehicles Soon',
+  'What Oustia’s Robotic Arm May Mean For Assembly Workers',
+  'What The Water Shortage Means For Some Communities',
+  'What Today’s Stock Market Means For Tomorrow’s Generation',
+  'Whistlety Product Recall Forces CEO’s Resignation',
+  'Why My Photos Sell (And You Can Do The Same)',
+  'Why Now Is The Best Time To Be A Pilot',
+  'Yes, You Should Complain More At Work',
+  'Zoomway Grand Prix Adds Zoomjax As Official Sponsor',
+];
