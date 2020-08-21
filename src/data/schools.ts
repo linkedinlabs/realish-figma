c3f5fb5c78d8d9d23bae981c1ee5f55b4a72f8e3
Grant Blakeman <hello@grantblakeman.com>
2020-08-21 14:29:14 -0600

    data - add “schools” set

---
+++ src/data/schools.ts
@@ -0,0 +1,113 @@
+/**
+ * @description School names.
+ *
+ * @kind constant
+ * @name schools
+ * @type {Array}
+ */
+export default [
+  {
+    name: 'Addison School of Business & Technology',
+    hasImage: true,
+  },
+  {
+    name: 'Agora School of Business',
+    hasImage: true,
+  },
+  {
+    name: 'Columbus Military College',
+    hasImage: true,
+  },
+  {
+    name: 'Corrie School of Environmental Science',
+    hasImage: true,
+  },
+  {
+    name: 'Faraday School of Engineering',
+    hasImage: true,
+  },
+  {
+    name: 'Fiftyfirst State University',
+    hasImage: true,
+  },
+  {
+    name: 'Fortuna Polytechnic',
+    hasImage: true,
+  },
+  {
+    name: 'Golden Bay State',
+    hasImage: true,
+  },
+  {
+    name: 'Joshua Carmona University',
+    hasImage: true,
+  },
+  {
+    name: 'Mary B. Curry College of Health Sciences',
+    hasImage: true,
+  },
+  {
+    name: 'Oxbow College Online',
+    hasImage: true,
+  },
+  {
+    name: 'Panorama Technical College',
+    hasImage: true,
+  },
+  {
+    name: 'Peninsula School of the Arts',
+    hasImage: true,
+  },
+  {
+    name: 'Pronghorn Polytechnic',
+    hasImage: true,
+  },
+  {
+    name: 'Redlands College of Agricultural Science',
+    hasImage: true,
+  },
+  {
+    name: 'Saint Richard’s College of Massachusetts',
+    hasImage: true,
+  },
+  {
+    name: 'Seal Coast Maritime College',
+    hasImage: true,
+  },
+  {
+    name: 'Southern Hall University',
+    hasImage: true,
+  },
+  {
+    name: 'Stratus Aeronautical Institute',
+    hasImage: true,
+  },
+  {
+    name: 'The Rockwell School',
+    hasImage: true,
+  },
+  {
+    name: 'Trinity Business School',
+    hasImage: true,
+  },
+  {
+    name: 'University of Southern Gulf',
+    hasImage: true,
+  },
+  {
+    name: 'Valleymouth Polytechnic',
+    hasImage: true,
+  },
+  {
+    name: 'Van Holden Academy of Art',
+    hasImage: true,
+  },
+  {
+    name: 'Wildwood School of Fine Arts',
+    hasImage: true,
+  },
+  {
+    name: 'Wright College of Oceanography',
+    hasImage: true,
+  },
+];
