77757173ac2ef950bf0e18dbc8232a4c5c943426
Grant Blakeman <hello@grantblakeman.com>
2020-08-21 14:36:08 -0600

    data - add “groups” set

---
+++ src/data/groups.ts
@@ -0,0 +1,22 @@
+/**
+ * @description Groups.
+ *
+ * @kind constant
+ * @name groups
+ * @type {Array}
+ */
+export default [
+  'Pacific Sales Professionals',
+  'Design for Social Impact',
+  'Saltwater Aquaculture',
+  'Off Grid Survival Enthusiasts',
+  'AR/VR/MR Marketing',
+  'Creative Writing & Self Publishing',
+  'Renewable Energy Recruiting',
+  'Association of Veterinary Dentists',
+  'Peninsula IT Professionals',
+  'Air Traffic Control Technology',
+  'Navigation & Guidance Control Systems',
+  'North Coast Real Estate Investors',
+  'Amateur Mead Brewing',
+];
