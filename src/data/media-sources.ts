068c617c12e5c990dc70d26dd253cbe2282d18ef
Grant Blakeman <hello@grantblakeman.com>
2020-08-21 14:48:59 -0600

    data - add “mediaSources” set

---
+++ src/data/media-sources.ts
@@ -0,0 +1,33 @@
+/**
+ * @description Media sources.
+ *
+ * @kind constant
+ * @name mediaSources
+ * @type {Array}
+ */
+export default [
+  'Agribusiness Today',
+  'Article Accelerator',
+  'Automaradar',
+  'Chronicle Daily',
+  'Countryside Examiner',
+  'Crow’s Nest',
+  'Enneagram',
+  'EqualOpposite',
+  'Gosssip',
+  'Grandfina.le',
+  'Hyperpolyglot',
+  'Investor.ly',
+  'Nimbus Aviation News',
+  'Online Observer',
+  'Post Reporter',
+  'PRQX News',
+  'Reportable',
+  'Technica Fama',
+  'Techyon',
+  'The 4ourfront',
+  'The Boxcutter',
+  'The Watts Weekly',
+  'TIL Insider',
+  'Vantage',
+];
