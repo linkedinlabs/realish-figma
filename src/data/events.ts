ccfc1205e39c6745933e334aac6660295a8bfc5f
Grant Blakeman <hello@grantblakeman.com>
2020-08-31 17:21:03 -0700

    data - events - retire some

--- src/data/events.ts
+++ src/data/events.ts
@@ -7,42 +7,6 @@
  */
 export default [
   {
-    name: 'Bachelorette After-hours',
-    hasImage: true,
-  },
-  {
-    name: 'Burger Tours',
-    hasImage: true,
-  },
-  {
-    name: 'Cosmic Wednesdays',
-    hasImage: true,
-  },
-  {
-    name: 'Florist Boutique Making',
-    hasImage: true,
-  },
-  {
-    name: 'Montessori Food Drive',
-    hasImage: true,
-  },
-  {
-    name: 'Open Mic Night',
-    hasImage: true,
-  },
-  {
-    name: 'Pollack Fall Carnival',
-    hasImage: true,
-  },
-  {
-    name: 'Root Vegetable Harvest',
-    hasImage: true,
-  },
-  {
-    name: 'Samba Party',
-    hasImage: true,
-  },
-  {
     name: 'The Climb',
     hasImage: true,
   },
