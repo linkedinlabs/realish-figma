03c8aa312782f121bd975935ccd40f92f4fce211
Grant Blakeman <hello@grantblakeman.com>
2020-09-09 13:01:23 -0700

    typescript - move Figma typings to package

---
+++ src/additionalTypes.d.ts
@@ -0,0 +1,46 @@
+declare global {
+  // Internal Declarations
+  type RealishAssignment =
+    'unassigned'
+    | 'alumni-company'
+    | 'alumni-school'
+    | 'article-title'
+    | 'avatar-company'
+    | 'avatar-company-media'
+    | 'avatar-event'
+    | 'avatar-group'
+    | 'avatar-newsletter'
+    | 'avatar-person'
+    | 'avatar-product'
+    | 'avatar-school'
+    | 'avatar-service'
+    | 'company'
+    | 'company-media'
+    | 'connections'
+    | 'connections-mutual'
+    | 'country'
+    | 'date'
+    | 'dateShort'
+    | 'degree-badge'
+    | 'domain'
+    | 'event'
+    | 'email'
+    | 'followers'
+    | 'group'
+    | 'industry'
+    | 'job-title'
+    | 'location'
+    | 'name'
+    | 'product'
+    | 'profile-headline'
+    | 'school'
+    | 'service'
+    | 'timestamp';
+
+  // Vendor Declarations
+  const selectMenu: window.selectMenu
+
+  // Figma’s typings in npm package @figma/plugin-typings
+} // declare global
+
+export {}
