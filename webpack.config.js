f814a1fdabcf586382f195765d3ff4a16946d226
gblakeman <gblakeman@linkedin.com>
2021-06-03 16:44:25 -0600

    webpack config - update inline sourcing config

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,7 @@ module.exports = (env, argv) => ({
       filename: 'webview.html',
       inlineSource: '.(js)$',
       chunks: ['ui'],
+      cache: false,
     }),
     new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
   ],
