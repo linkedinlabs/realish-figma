21b074037b74b4f4d2b5a14373ea4bb8942b1e27
gblakeman <gblakeman@linkedin.com>
2021-06-25 14:40:46 -0700

    views - stub in base

---
+++ src/views/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  // import { afterUpdate, beforeUpdate } from 'svelte';
+  import FontPreload from './FontPreload';
+  // import BlankState from './BlankState';
+
+  // export let items;
+  // export let newSessionKey = null;
+
+  let bodyHeight = 0;
+  // let wasBodyHeight = 0;
+
+  // beforeUpdate(() => {
+  // });
+
+  // afterUpdate(() => {
+  // });
+</script>
+
+<!-- compile options -->
+<svelte:options accessors={true}/>
+
+<!-- core layout -->
+<div bind:offsetHeight={bodyHeight}>
+  <FontPreload/>
+  <h1>hello</h1>
+</div>
