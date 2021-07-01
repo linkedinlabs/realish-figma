eb81f041ff21cf5a9c897109a28742d008b12616
gblakeman <gblakeman@linkedin.com>
2021-06-30 18:25:19 -0700

    views - list - set for images

--- src/views/Item.svelte
+++ src/views/Item.svelte
@@ -3,7 +3,7 @@
   import ButtonRemix from './forms-controls/ButtonRemix';
   import ButtonRestore from './forms-controls/ButtonRestore';
   import FormUnit from './forms-controls/FormUnit';
-  import { textAssignmentsSelect } from './stores';
+  import { shapeAssignmentsSelect, textAssignmentsSelect } from './stores';
 
   export let assignment = 'unassigned';
   export let isImage = false;
@@ -12,12 +12,58 @@
   export let originalText = null;
   export let proposedText = null;
   export let rounded = 'none';
+
+  const layerType = (currentIsImage) => isImage ? 'shape' : 'text';
 </script>
 
-{#if isImage}
-  <li>{originalText}</li>
-{:else}
-  <li class="text-layer-holder locked">
+<li class={`${layerType(isImage)}-layer-holder locked`}>
+  {#if isImage}
+    <!-- proposed content widget -->
+    <span class={`shape-widget new-shape rounded-${rounded}`}>
+      <span class="shape">
+        {proposedText}
+      </span>
+    </span>
+
+    <span class="column-holder">
+      <span class="row">
+        <!-- original shape widget -->
+        <span class={`shape-widget original-shape rounded-${rounded}`}>
+          <span class="inline-control reset-control">
+            <ButtonRestore disabled={originalText === proposedText}/>
+          </span>
+          <span class="shape">
+            {originalText}
+          </span>
+        </span>
+
+        <!-- layer assignment select -->
+        <FormUnit
+          className="layer-assignment-control select"
+          kind="inputSelect"
+          options={isImage ? $shapeAssignmentsSelect : $textAssignmentsSelect}
+          labelText="Assigned text type"
+          nameId={`${itemId}-assignment-type`}
+          placeholder="Leave empty to use browser default"
+          selectWatchChange={true}
+          bind:value={assignment}
+        />
+      </span>
+
+      <span class="row">
+        <span class="inline-control remix-control">
+          <ButtonRemix disabled={assignment === 'unassigned'}/>
+        </span>
+
+        <!-- locking control -->
+        <span class="locking-control">
+          <ButtonLock
+            isLocked={isLocked}
+          />
+        </span>
+      </span>
+    </span>
+  {:else}
     <span class="row">
       <!-- original text widget -->
       <span class="text-widget original-text">
@@ -33,7 +79,7 @@
       <FormUnit
         className="layer-assignment-control select"
         kind="inputSelect"
-        options={$textAssignmentsSelect}
+        options={isImage ? $shapeAssignmentsSelect : $textAssignmentsSelect}
         labelText="Assigned text type"
         nameId={`${itemId}-assignment-type`}
         placeholder="Leave empty to use browser default"
@@ -60,5 +106,5 @@
         />
       </span>
     </span>
-  </li>
-{/if}
+  {/if}
+</li>
