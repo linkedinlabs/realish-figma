ff6ff5d24fe16a2061ecf977b42c840a388f7e57
gblakeman <gblakeman@linkedin.com>
2021-07-02 16:25:58 -0700

    views - footer - hook in main actions

--- src/views/FooterBar.svelte
+++ src/views/FooterBar.svelte
@@ -1,6 +1,13 @@
 <script>
   import RealishLogo from '../assets/images/RealishLogo';
   import FigmaButton from './forms-controls/FigmaButton';
+  import { sendMsgToMain } from '../Tools';
+
+  const runAction = (actionToRun) => {
+    const action = actionToRun;
+    const payload = null;
+    sendMsgToMain(action, payload);
+  };
 </script>
 
 <footer id="actions">
@@ -10,10 +17,14 @@
   <div class="actions-holder">
     <FigmaButton
       className="button--margin-right"
+      action="remix-all"
+      on:handleAction={(customEvent) => runAction(customEvent.detail)}
       text="Randomize"
       type="secondary"
     />
     <FigmaButton
+      action="submit"
+      on:handleAction={(customEvent) => runAction(customEvent.detail)}
       text="Apply"
       type="primary"
     />
