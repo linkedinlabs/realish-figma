9ebc7ef16d28f3db99b28983440783cee5901048
gblakeman <gblakeman@linkedin.com>
2021-07-14 15:02:49 -0700

    test pkg - stub in mocha for testing

---
+++ test-pkg/test/test.js
@@ -0,0 +1,52 @@
+const { expect } = require('chai');
+const index = require('../dist/index.js');
+
+require('mocha-sinon');
+
+describe('hello world test', () => {
+  beforeEach('initiate console', function () {
+    this.sinon.stub(console, 'log');
+  });
+
+  it('should call console.log', () => {
+    index.helloWorld();
+    expect(console.log.calledOnce).to.equal(true); // eslint-disable-line no-console
+  });
+
+  it('should print “hello world” in console.log', () => {
+    index.helloWorld();
+    expect(console.log.calledWith('hello world')).to.equal(true); // eslint-disable-line no-console
+  });
+});
+
+describe('simple validate test', () => {
+  it('should return true for null string', () => {
+    const string = null;
+    const result = index.isStringNullOrEmpty(string);
+    expect(result).to.equal(true);
+  });
+
+  it('should return true for undefined string', () => {
+    const string = undefined;
+    const result = index.isStringNullOrEmpty(string);
+    expect(result).to.equal(true);
+  });
+
+  it('should return true for empty string', () => {
+    const string = '';
+    const result = index.isStringNullOrEmpty(string);
+    expect(result).to.equal(true);
+  });
+
+  it('should return true for whitespace string', () => {
+    const string = ' ';
+    const result = index.isStringNullOrEmpty(string);
+    expect(result).to.equal(true);
+  });
+
+  it('should return false for non-empty string', () => {
+    const string = 'test';
+    const result = index.isStringNullOrEmpty(string);
+    expect(result).to.equal(false);
+  });
+});
